a69ce60442c7bd84ffddda28473de9370eb27bc6
Jan Brudný <xbrudn02@stud.fit.vutbr.cz>
2022-12-04 14:40:09 +0100

    remove build files

--- build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/jankubala/Downloads/IMSProject
-# Build directory: /home/jankubala/Downloads/IMSProject/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
